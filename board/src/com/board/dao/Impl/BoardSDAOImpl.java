0141fddf795919ad50f36074f551ba532732d860
jung <java-21@java-21-PC>
2017-11-09 15:57:46 +0900

    board complete

---
+++ board/src/com/board/dao/Impl/BoardSDAOImpl.java
@@ -0,0 +1,15 @@
+package com.board.dao.Impl;
+
+import java.sql.SQLException;
+
+import com.board.dao.BoardDAO;
+
+public class BoardSDAOImpl implements BoardDAO {
+
+	@Override
+	public void insertBoard(String board) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
